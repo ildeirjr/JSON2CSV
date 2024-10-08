5557fb1f3c7c9149a3246b920cede26f19977f12
Ildeir de Oliveira Eler Junior <ildeir@gmail.com>
2024-10-07 23:34:03 -0300

    Add table and some fixes.

--- scripts/converter.js
+++ scripts/converter.js
@@ -1,13 +1,25 @@
-var jsonObj = null
+function emptyTable() {
+    $("#csv-table-header").empty()
+    $("#csv-table-body").empty()
+}
+
+function displayTable(toggle) {
+    if (toggle == true)
+        $("#csv-table").css("display","block")
+    else
+        $("#csv-table").css("display","none")
+}
 
 function emptyFields() {
     $("#txtJSON").val("")
     $("#txtCSV").val("")
+    displayTable(false)
+    emptyTable()
 }
 
 function validate(txtJSON) {
     try {
-        jsonObj = JSON.parse(txtJSON)
+        JSON.parse(txtJSON)
         return true
     } catch(e) {
         console.log(e.message)
@@ -19,61 +31,64 @@ function convert() {
     var txtJSON = $("#txtJSON").val()
 
     var txtCSV = ""
-
     var columns = ""
     var lines = []
 
     $("#txtCSV").val("")
 
     if(validate(txtJSON)) {
-        //console.log(jsonObj)
+        emptyTable()
 
         txtJSON = txtJSON.replaceAll("\n","")
+        var objMatches = txtJSON.match(/\{.*?\}/gm)
 
-        var arrayMatches = txtJSON.match(/\[.*?\]/gm);
+        var column = []
 
-        arrayMatches.forEach(array => {
-            var objMatches = array.match(/\{.*?\}/gm)
+        // Varrer objetos JSON para obter todas as colunas distintas
+        objMatches.forEach(obj => {
+            obj = obj.replace("{","").replace("}","").replaceAll(" ","")
+            var arrKeyValue = obj.split(",")
 
-            //console.log(objMatches)
-
-            var column = []
-
-            objMatches.forEach(obj => {
-                //console.log(obj)
-
-                obj = obj.replace("{","").replace("}","").replaceAll(" ","")
-                
-                var arrKeyValue = obj.split(",")
-                
-                var line = []
-
-                arrKeyValue.forEach(pairKeyValue => {
-                    var key = pairKeyValue.split(":")[0].replaceAll('"',"")
-                    var value = pairKeyValue.split(":")[1].replaceAll('"',"")
-                    
-                    //console.log("KEY:", key)
-                    //console.log("VALUE:", value)
-
-                    if(!column.includes(key))
-                        column.push(key)
-
-                    line.push(value)
-                });
-
-                lines.push(line)
-            });
-
-            columns = column.join(";")
-            txtCSV = columns
-
-            lines.forEach(line => {
-                txtCSV += "\n" + line.join(";")
+            arrKeyValue.forEach(pairKeyValue => {
+                var key = pairKeyValue.split(":")[0].replaceAll('"',"")
+                if(!column.includes(key)) {
+                    column.push(key)
+                    $("#csv-table-header").append('<th scope="col">' + key + '</th>')
+                }
             });
         });
+
+        var line = []
+
+        // Varrer objetos JSON novamente para associar os valores às respectivas colunas
+        objMatches.forEach((obj,idx) => {
+            line = Array.from(' '.repeat(column.length))
+            $("#csv-table-body").append(`<tr id="line-${idx}">` + '<td></td>'.repeat(column.length) + '</tr>')
+
+            obj = obj.replace("{","").replace("}","").replaceAll(" ","")
+            var arrKeyValue = obj.split(",")
+
+            arrKeyValue.forEach(pairKeyValue => {
+                var key = pairKeyValue.split(":")[0].replaceAll('"',"")
+                var value = pairKeyValue.split(":")[1].replaceAll('"',"")
+                
+                var columnIdx = column.findIndex((element) => element == key)
+                line[columnIdx] = value
+
+                $(`#line-${idx} td`).eq(columnIdx).html(value)
+            });
+
+            lines.push(line)
+        });
+
+        columns = column.join(",")
+        txtCSV = columns
+        lines.forEach(line => {
+            txtCSV += "\n" + line.join(",")
+        });
         
-        console.log(txtCSV)
         $("#txtCSV").val(txtCSV)
+        displayTable(true)
     } else {
         alert("O text JSON não é válido!")
     }
